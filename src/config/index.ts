da8c3c010bf9bf9aab3badc2dab791ec70c51a7a
Nguyen Tran Trung <nguyentt@smartosc.com>
2025-07-03 18:28:12 +0700

    update

---
+++ src/config/index.ts
@@ -0,0 +1,1 @@
+export const MAIN_URL = process.env.NEXT_PUBLIC_MAIN_URL || "";
--- src/hooks/products/useProducts.ts
+++ src/hooks/products/useProducts.ts
@@ -1,12 +1,14 @@
 "use client";
 import { IProduct } from "@/components/products";
+import { MAIN_URL } from "@/config";
 import { useEffect, useState } from "react";
 
 export default function useProducts() {
   const [products, setProducts] = useState<IProduct[]>([]);
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch("http://localhost:3000/api/get-products");
+      if (!MAIN_URL) return;
+      const res = await fetch(`${MAIN_URL}/api/get-products`);
       if (!res.ok) throw new Error("Failed to fetch products");
       const data: IProduct[] = await res.json();
 
